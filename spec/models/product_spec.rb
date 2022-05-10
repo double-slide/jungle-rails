8c51d175ade4b316ffd3c37065a7d9ac3baeffed
double-slide <93881358+double-slide@users.noreply.github.com>
2022-05-10 02:46:37 +0000

    Add specs for for testing product validation

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,15 +2,41 @@ require 'rails_helper'
 
 RSpec.describe Product, type: :model do
   describe 'Validations' do
-    
-    context "given '4'" do
-      it "returns 4" do
-        expect(StringCalculator.add("4")).to eql(4)
-      end
+
+
+    before do
+      @test_category = Category.new(:name => "Tester")
     end
 
-    
 
-  
+    it "saves with all four fields set" do
+      test_product = Product.new({ :name => "Test", :price => 100, :quantity => 1, :category => @test_category })
+      expect(test_product).to be_valid
+    end
+
+
+    it "does not save if name is excluded" do
+      test_product = Product.new({ :price => 100, :quantity => 1, :category => @test_category })
+      expect(test_product).not_to be_valid
+    end
+
+
+    it "does not save if price is excluded" do
+      test_product = Product.new({ :name => "Test", :quantity => 1, :category => @test_category })
+      expect(test_product).not_to be_valid
+    end
+
+
+    it "does not save if quantity is excluded" do
+      test_product = Product.new({ :name => "Test", :price => 100, :category => @test_category })
+      expect(test_product).not_to be_valid
+    end
+
+
+    it "does not save if category is excluded" do
+      test_product = Product.new({ :name => "Test", :price => 100, :quantity => 1})
+      expect(test_product).not_to be_valid
+    end
+
   end
 end
