774e1bd28bd54cc45858300f8f2b37ea209e0969
double-slide <93881358+double-slide@users.noreply.github.com>
2022-05-10 00:47:37 +0000

    Start new test

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,9 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
+  describe 'Validations' do
+    
+    context "given '4'" do
+      it "returns 4" do
+        expect(StringCalculator.add("4")).to eql(4)
+      end
+    end
 
+    
 
-
-
-
+  
+  end
 end
